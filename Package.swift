95a796dc95c259a7932f5f104b46507922aa623a
Geordie Jay <geojay@gmail.com>
2025-10-22 19:13:23 +0200

    Clean up for clarity

--- Package.swift
+++ Package.swift
@@ -1,30 +1,15 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TestPM",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "TestPM",
-            targets: ["TestPM"]
-        ),
-        .library(
-            name: "Broken",
-            targets: ["Broken"]
-        ),
+        .library(name: "TestPM", targets: ["TestPM"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "TestPM"
-        ),
-        .target(
-            name: "Broken"
-        ),
+        .target(name: "TestPM"),
+        .target(name: "Broken"),
         .testTarget(
             name: "TestPMTests",
             dependencies: ["TestPM"]
